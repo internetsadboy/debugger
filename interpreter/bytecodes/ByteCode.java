ee93e2f5db942cbc0de8584cda76ac5bb2565070
jared <jaredhalpert90@gmail.com>
2014-04-30 11:44:51 -0700

    recopying

--- interpreter/bytecodes/ByteCode.java
+++
@@ -1,9 +0,0 @@
-package interpreter.bytecodes;
-
-import java.util.*;
-import interpreter.*;
-
-public abstract class ByteCode {
-    public abstract void execute(VirtualMachine vm);
-    public abstract void init(Vector<String> args);
-}
